b22702e606bfaec7f200ac74925ba9528d50de56
norapat pumyoo <niabzagamertv16325@gmail.com>
2021-11-16 11:40:07 +0700

    week 2

--- src/views/Tab3.vue
+++ src/views/Tab3.vue
@@ -2,17 +2,17 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 3</ion-title>
+        <ion-title>ผู้ใช้</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Tab 3</ion-title>
+          <ion-title size="large">ผู้ใช้</ion-title>
         </ion-toolbar>
       </ion-header>
       
-      <ExploreContainer name="Tab 3 page" />
+      <ExploreContainer name="ผู้ใช้" />
     </ion-content>
   </ion-page>
 </template>
